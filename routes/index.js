ee38d3a98dcc957b701bde0ab2176854b48bc921
Jason Jiménez Cruz <jasonjimenezcruz@gmail.com>
2021-12-18 13:20:01 +0100

    Added changes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,17 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+/* GET API STATUS */
+router.get("/api/status", (req, res, next) => {
+  // Con esto le decimos al API que nos va a devolver un JSON
+  res.setHeader('Content-Type', 'application/json');
+  // TODO: Toda la lógica iría aquí dentro
+
+  // Esta última instrucción devolverá el objeto como un JSON
+  res.end(JSON.stringify({
+    name: "ZAP",
+    version: "v1"
+  }));
+});
+
 module.exports = router;
